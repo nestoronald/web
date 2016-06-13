a0941f328d6bb8ce6336dbfd0a49920f64a29877
Ronald Loyola <rloyola@cenepred.gob.pe>
2016-06-13 16:11:33 -0500

    noticias y footer v6

--- wp-content/themes/cenepred/template-home.php
+++ wp-content/themes/cenepred/template-home.php
@@ -125,7 +125,7 @@ cursor:pointer; cursor: hand;
 			
 			       $show_posts = new WP_Query( $args ); 
 				?>				
-					<?php if ( $show_posts->have_posts() ) :   var_dump($show_posts->have_posts()) ; ?>
+					<?php if ( $show_posts->have_posts() ) :   var_dump($show_posts->the_post()) ; ?>
 
 						<?php /* Start the Loop */ ?>
 						<?php while ( $show_posts->have_posts() ) : $show_posts->the_post(); ?>
