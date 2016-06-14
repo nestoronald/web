a2702b82f51c35bce2d42492c4c3e41c23038c9b
Ronald Loyola <rloyola@cenepred.gob.pe>
2016-06-14 10:46:03 -0500

    publicaciones v4

--- wp-content/themes/cenepred/template-home.php
+++ wp-content/themes/cenepred/template-home.php
@@ -213,19 +213,19 @@ cursor:pointer; cursor: hand;
 		<div class="publicaciones">
 		<h3>Publicaciones</h3>
 			<ul class="slide-pub">
-		            <li class="itemposts large"><div class="items  ">
+		            <li class="itemposts"><div class="items  ">
 		            <a href="http://192.168.2.20/web/mapa-de-evaluacion-de-riesgo-2016/" target="_blank" title="Publicación 1" >
 		            <img alt="Presidencia del Consejo de Ministros" src="<?php echo get_template_directory_uri(); ?>/img/publicaciones/pub-1.png" /></a></div>
 		            </li>
-		            <li class="itemposts large"><div class="items  ">
+		            <li class="itemposts"><div class="items  ">
 		            <a href="http://192.168.2.20/web/mapa-de-evaluacion-de-riesgo-2016/" target="_blank" title="Publicación 2" >
 		            <img alt="Secretaria de Gestion del Riesgo de Desastres" src="<?php echo get_template_directory_uri(); ?>/img/publicaciones/pub-2.png" /></a></div>
 		            </li>
-		            <li class="itemposts large"><div class="items  ">
+		            <li class="itemposts"><div class="items  ">
 		            <a href="http://192.168.2.20/web/mapa-de-evaluacion-de-riesgo-2016/" target="_blank" title="Publicación 1" >
 		            <img alt="Presidencia del Consejo de Ministros" src="<?php echo get_template_directory_uri(); ?>/img/publicaciones/pub-1.png" /></a></div>
 		            </li>
-		            <li class="itemposts large"><div class="items  ">
+		            <li class="itemposts last"><div class="items  ">
 		            <a href="http://192.168.2.20/web/mapa-de-evaluacion-de-riesgo-2016/" target="_blank" title="Publicación 2" >
 		            <img alt="Secretaria de Gestion del Riesgo de Desastres" src="<?php echo get_template_directory_uri(); ?>/img/publicaciones/pub-2.png" /></a></div>
 		            </li>		           
