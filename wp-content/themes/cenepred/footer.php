766b25dc13263ed334e87dd70d1793c50d588fa3
Ronald N <rloyola@cenepred.gob.pe>
2016-07-04 10:25:48 -0500

    margen simetrico main-conten

--- wp-content/themes/cenepred/footer.php
+++ wp-content/themes/cenepred/footer.php
@@ -22,11 +22,7 @@
             <a href="http://sgrd.pcm.gob.pe/" target="_blank" title="Secretaria de Gestion del Riesgo de Desastres" >
             <img alt="Secretaria de Gestion del Riesgo de Desastres" src="<?php echo get_template_directory_uri(); ?>/img/instituciones/sec-ges-riesgos-desastres.png" /></a></div>
             </li>
-
-            <li class="itemposts medium-1"><div class="items">
-            <a href="http://sgrd.pcm.gob.pe/" target="_blank" title="Indeci" >
-            <img alt="Indeci" src="<?php echo get_template_directory_uri(); ?>/img/instituciones/indeci.png" /></a></div>
-            </li>
+            
 
              <li class="itemposts small-3"><div class="items"><a href="http://www.senamhi.gob.pe/" title="SENAHMI" ><img alt="SENAHMI" src="<?php echo get_template_directory_uri(); ?>/img/instituciones/senamhi.png" />
             </a></div>
@@ -72,32 +68,32 @@
         </ul> 
         
         <a title="Siguiente" id="nextfeatured-properties" href="#" class="next btn_next2 animationAll z2">Siguiente<span></span></a>
-	    <a title="Anterior" id="prevfeatured-properties" href="#" class="prev btn_prev2 animationAll z2">Anterior<span></span></a>
+        <a title="Anterior" id="prevfeatured-properties" href="#" class="prev btn_prev2 animationAll z2">Anterior<span></span></a>
     </div>
 </section>
 </div>
 <footer id="footer"> 
     <div class="container">
         <ul class="sidebar group">
-        	<?php if ( ! dynamic_sidebar( 'Footer' ) ) : ?>
-        	<li class="widget">
-        		<h3 class="widgettitle"><?php _e('Just a Static Widget', 'blakzr'); ?></h3>
-        		<p><?php  _e('This is not a real widget. Just a placeholder. You can put any other available widgets in this area. Try it, what are you waiting for?', 'blakzr'); ?></p>
-        	</li>
-        	 	<?php endif; ?>
+            <?php if ( ! dynamic_sidebar( 'Footer' ) ) : ?>
+            <li class="widget">
+                <h3 class="widgettitle"><?php _e('Just a Static Widget', 'blakzr'); ?></h3>
+                <p><?php  _e('This is not a real widget. Just a placeholder. You can put any other available widgets in this area. Try it, what are you waiting for?', 'blakzr'); ?></p>
+            </li>
+                <?php endif; ?>
         </ul>
     </div>
 </footer>
-	<?php if ( '' != get_option( 'blakzr_tracking_code', '' ) ) :
-		echo get_option( 'blakzr_tracking_code' );
-	endif; ?>
-	<?php
-	   /* Always have wp_footer() just before the closing </body>
-	    * tag of your theme, or you will break many plugins, which
-	    * generally use this hook to reference JavaScript files.
-	    */
-	    wp_footer();
-	?>
+    <?php if ( '' != get_option( 'blakzr_tracking_code', '' ) ) :
+        echo get_option( 'blakzr_tracking_code' );
+    endif; ?>
+    <?php
+       /* Always have wp_footer() just before the closing </body>
+        * tag of your theme, or you will break many plugins, which
+        * generally use this hook to reference JavaScript files.
+        */
+        wp_footer();
+    ?>
 
 </body>
 </html>
